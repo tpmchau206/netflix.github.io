c842293cf1b1c605d189c6311568ced133b81ce9
Chau Tran <nhuanquang00@gmail.com>
2022-02-12 03:15:47 +0700

    Message

--- js/slick.js
+++ js/slick.js
@@ -1,28 +1,51 @@
 $(document).ready(function () {
     $(".row-container").slick({
-        centerMode: true,
+        //     slidesToShow: 5,
+        //     slidesToScroll: 5,
+        //     // centerMode: true,
+        //     prevArrow: '<button type="button" class="slick-prev"><ion-icon name="chevron-back-outline"></ion-icon></button>',
+        //     nextArrow: '<button type="button" class="slick-next"><ion-icon name="chevron-forward-outline"></ion-icon></button>',
+
+        // });
+
+        // var filtered = false;
+
+        // $('.js-filter').on('click', function () {
+        //     if (filtered === false) {
+        //         $('.filtering').slick('slickFilter', ':even');
+        //         $(this).text('Unfilter Slides');
+        //         filtered = true;
+        //     } else {
+        //         $('.filtering').slick('slickUnfilter');
+        //         $(this).text('Filter Slides');
+        //         filtered = false;
+        //     }
+
+        // centerMode: true,
         centerPadding: '60px',
         slidesToShow: 5,
+        slidesToScroll: 5,
         draggable: false,
+        infinite: true,
         prevArrow: '<button type="button" class="slick-prev"><ion-icon name="chevron-back-outline"></ion-icon></button>',
         nextArrow: '<button type="button" class="slick-next"><ion-icon name="chevron-forward-outline"></ion-icon></button>',
         responsive: [
             {
-                breakpoint: 768,
+                breakpoint: 900,
                 settings: {
                     arrows: false,
-                    centerMode: true,
                     centerPadding: '40px',
-                    slidesToShow: 5
+                    slidesToScroll: 3,
+                    slidesToShow: 3
                 }
             },
             {
                 breakpoint: 480,
                 settings: {
                     arrows: false,
-                    centerMode: true,
                     centerPadding: '40px',
-                    slidesToShow: 5
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
                 }
             }
         ]
